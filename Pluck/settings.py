0b23714bc51bb1ff3115f9ba7d25e64d659873e8
Rajat Hans <rajathans9@gmail.com>
2017-12-07 22:24:32 +0530

    Added requirements file

--- Pluck/settings.py
+++ Pluck/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'Pluck.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'IST'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 

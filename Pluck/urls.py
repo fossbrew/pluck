1ee97dc536752a17c6b86999781fb464443e130f
Rajat Hans <rajathans9@gmail.com>
2017-12-09 16:26:09 +0530

    Refactored a lot of stuff

--- Pluck/urls.py
+++ Pluck/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('pluckapp.urls')),
+    url(r'^', include('pluck.apps.core.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- pluck/apps/core/apps.py
+++ pluck/apps/core/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class PluckappConfig(AppConfig):
-    name = 'pluckapp'
+    name = 'pluck'
